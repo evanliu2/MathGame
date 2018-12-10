bf0e6ee0ee48aef9f8179b3579210473cbdf9332
Evan L <evan.x.liu@gmail.com>
2018-12-10 09:20:17 -0800

    Implemented calculation logic

--- app/src/main/java/com/example/maththing/MainActivity.java
+++ app/src/main/java/com/example/maththing/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.maththing;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -14,7 +16,27 @@ public class MainActivity extends AppCompatActivity {
     private Button row1col1, row1col2, row1col3, row1col4, row2col1, row2col2, row2col3, row2col4, row3col1, row3col2, row3col3, row3col4, reset, add, subt, mult, div;
     private int target;
     private Boolean redo = true;
-    private TextView targetNum;
+    private TextView targetNum, userInput;
+    private Boolean numClicked1 = false;
+    private Boolean numClicked2 = false;
+    private Boolean operationClicked = false;
+    private int num1;
+    private int num2;
+    private int operation;
+    private int randNumRow1Col1;
+    private int randNumRow1Col2;
+    private int randNumRow1Col3;
+    private int randNumRow1Col4;
+    private int randNumRow2Col1;
+    private int randNumRow2Col2;
+    private int randNumRow2Col3;
+    private int randNumRow2Col4;
+    private int randNumRow3Col1;
+    private int randNumRow3Col2;
+    private int randNumRow3Col3;
+    private int randNumRow3Col4;
+
+
 
     // modify based on level difficulties
     private int operationNum = 4;
@@ -28,6 +50,346 @@ public class MainActivity extends AppCompatActivity {
         wireWidgets();
         createComponents();
 
+        row1col1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               if(numClicked1 == false)
+               {
+                   num1 = randNumRow1Col1;
+                   numClicked1 = true;
+               }
+
+               else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+               {
+                   num2 = randNumRow1Col1;
+                   userInput.setText(calculate(num1, num2, operation));
+                   num1 = 0;
+                   num2 = 0;
+                   numClicked1 = false;
+                   numClicked2 = false;
+               }
+
+            }
+        });
+
+        row1col2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow1Col2;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow1Col2;
+                    userInput.setText(calculate(num1, num2, operation) + "");
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row1col3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow1Col3;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow1Col3;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row1col4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow1Col4;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow1Col4;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row2col1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow2Col1;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow2Col1;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row2col2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow2Col2;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow2Col2;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row2col3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow2Col3;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow2Col3;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row2col4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow2Col4;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow2Col4;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row3col1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow3Col1;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow3Col1;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row3col2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow3Col2;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow3Col2;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row3col3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow3Col3;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow3Col3;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        row3col4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(numClicked1 == false)
+                {
+                    num1 = randNumRow3Col4;
+                    numClicked1 = true;
+                }
+
+                else if(numClicked1 == true && operationClicked == true && numClicked2 == false)
+                {
+                    num2 = randNumRow3Col4;
+                    userInput.setText(calculate(num1, num2, operation));
+                    num1 = 0;
+                    num2 = 0;
+                    numClicked1 = false;
+                    numClicked2 = false;
+                }
+
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (numClicked1 == true && operationClicked == false && numClicked2 == false)
+                {
+                    operation = 1;
+                    operationClicked = true;
+                }
+            }
+        });
+
+        subt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (numClicked1 == true && operationClicked == false && numClicked2 == false)
+                {
+                    operation = 2;
+                    operationClicked = true;
+                }
+            }
+        });
+
+        mult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (numClicked1 == true && operationClicked == false && numClicked2 == false)
+                {
+                    operation = 3;
+                    operationClicked = true;
+                }
+
+            }
+        });
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (numClicked1 == true && operationClicked == false && numClicked2 == false)
+                {
+                    operation = 4;
+                    operationClicked = true;
+                }
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                userInput.setText("");
+                num1 = 0;
+                num2 = 0;
+                operation = 4;
+                numClicked1 = false;
+                numClicked2 = false;
+                operationClicked = false;
+
+            }
+        });
     }
 
     private void createComponents() {
@@ -57,6 +419,19 @@ public class MainActivity extends AppCompatActivity {
         row3col3.setText("" + numbers.get(10));
         row3col4.setText("" + numbers.get(11));
 
+        randNumRow1Col1 = numbers.get(0);
+        randNumRow1Col2 = numbers.get(1);
+        randNumRow1Col3 = numbers.get(2);
+        randNumRow1Col4 = numbers.get(3);
+        randNumRow2Col1 = numbers.get(4);
+        randNumRow2Col2 = numbers.get(5);
+        randNumRow2Col3 = numbers.get(6);
+        randNumRow2Col4 = numbers.get(7);
+        randNumRow3Col1 = numbers.get(8);
+        randNumRow3Col2 = numbers.get(9);
+        randNumRow3Col3 = numbers.get(10);
+        randNumRow3Col4 = numbers.get(11);
+
     }
 
     private void wireWidgets() {
@@ -79,9 +454,41 @@ public class MainActivity extends AppCompatActivity {
         mult = findViewById(R.id.button_main_multiplication);
         div = findViewById(R.id.button_main_division);
         targetNum = findViewById(R.id.textView_main_targetNum);
+        userInput = findViewById(R.id.textView_main_userinput);
 
 
     }
 
+    private int specialNum = (int)(Math.pow(range*1.0, operationNum*1.0)+1);
+
+    private int calculate(int num1, int num2, int operation) {
+        if (operation == 1) {
+            return num1 + num2;
+        } else if (operation == 2) {
+            if (num1 - num2 >= 0) {
+                return num1 - num2;
+            } else {
+                return specialNum;
+            }
+        } else if (operation == 3) {
+            return num1 * num2;
+        } else if (operation == 4) {
+            if (num1 >= num2 && num1%num2 == 0) {
+                return num1/num2;
+            } else {
+                return specialNum;
+            }
+        }
+        return specialNum;
+    }
+
+
 
 }
+
+//https://crunchify.com/how-to-create-simple-calculator-android-app-using-android-studio/
+//calculator logic
+//https://stackoverflow.com/questions/1397361/how-do-i-restart-an-android-activity
+//possible reset button
+//https://www.mkyong.com/java/java-convert-string-to-int/
+//string to int logic
